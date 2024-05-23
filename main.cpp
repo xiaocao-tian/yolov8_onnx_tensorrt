9fe509c52b62e28c751c3dfaca4268e031607382
xiaocao-tian <65889782+xiaocao-tian@users.noreply.github.com>
2024-05-23 22:56:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,1 @@
-#include "yolo.h"
 
-int main() {
-	std::string engine_file = "../weights/yolov8n.engine";
-	std::string class_file  = "../weights/classes.txt";
-	float conf_threshold = 0.25;
-	float NMS_threshold = 0.45;
-
-	YOLO YOLO(engine_file, class_file, conf_threshold, NMS_threshold);
-	YOLO.Inference();
-
-	return 0;
-}
